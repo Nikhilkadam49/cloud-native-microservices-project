d4d3e865819bc4cb61293b5f0fe4baf5564a5ff5
Nikhilkadam49 <nikhilkadam4901@gmail.com>
2025-06-16 00:03:34 +0530

    v1

---
+++ k8s/eks.tf
@@ -0,0 +1,28 @@
+module "eks" {
+  source          = "terraform-aws-modules/eks/aws"
+  version         = "20.8.4"
+
+  cluster_name    = var.cluster_name
+  cluster_version = "1.29"
+  subnet_ids      = module.vpc.private_subnets
+  vpc_id          = module.vpc.vpc_id
+
+  eks_managed_node_group_defaults = {
+    ami_type = "AL2_x86_64"
+  }
+
+  eks_managed_node_groups = {
+    default = {
+      min_size     = 1
+      max_size     = 2
+      desired_size = 2
+
+      instance_types = ["t3.medium"]
+    }
+  }
+
+  tags = {
+    Terraform = "true"
+    Project   = "cloud-native"
+  }
+}
